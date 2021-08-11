9a4f8aaa052de0c9dc0289a66b12845c09ac12e4
santosh sougrakpam <santoshsougrakpam@santoshs-MacBook-Pro.local>
2021-08-11 19:00:33 +0100

    First commit

---
+++ src/main/java/com/example/jenkinsnexusdemo/JenkinsNexusDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsnexusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsNexusDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsNexusDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsnexusdemo/JenkinsNexusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsnexusdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsNexusDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
